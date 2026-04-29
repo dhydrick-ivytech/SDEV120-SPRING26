be9d5b727dc2d4cc76d3af519a5d73279b06183f
dhydrick-ivytech <dhydrick@ivytech.edu>
2026-04-28 21:19:42 -0400

    Create Loops.py

---
+++ Projects/Module 7/Loops.py
@@ -0,0 +1,18 @@
+'''#I dont want to print the number 3, but I want to print all the other numbers from 1 to 5.
+
+# for i in range(1,31):
+#     if i %3 == 0:
+#         continue
+#     else:
+#         print(i)    
+
+# for i in range(1,31):
+#     if i == 17:
+#         break
+#     else:
+#         print(i)'''
+
+scores=[90, 85, 78, 92, 88, 60, 55, 70, 82, 95]
+for grade in scores:
+    if grade > 60:
+        print(grade)
